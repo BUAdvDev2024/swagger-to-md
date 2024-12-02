a38c986726e33602247a68998e7f19cad067672c
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2024-12-02 16:07:51 +0000

    Feature: implemented printing endpoint names with methods

--- output.md
+++ output.md
@@ -6,30 +6,20 @@ Number of Paths: 7
 * [Disciplinary](#disciplinary)
 	* get-employees-disciplinaries
 	* create-disciplinary
-	* register
-	* terminated-employee
-	* all
-	* create-role
-	* get-roles
 * [Profile](#profile)
-	* get-employees-disciplinaries
-	* create-disciplinary
 	* register
 	* terminated-employee
 	* all
-	* create-role
-	* get-roles
 * [Role](#role)
-	* get-employees-disciplinaries
-	* create-disciplinary
-	* register
-	* terminated-employee
-	* all
 	* create-role
 	* get-roles
-## <a name="disciplinary"></a> Disciplinary
-Some detailed documentation for this group will go here.
-## <a name="profile"></a> Profile
-Some detailed documentation for this group will go here.
-## <a name="role"></a> Role
-Some detailed documentation for this group will go here.
+### <a name="disciplinary"></a> Disciplinary
+#### <a name="/disciplinary/get-employees-disciplinaries"></a>/disciplinary/get-employees-disciplinaries `GET` 
+#### <a name="/disciplinary/create-disciplinary"></a>/disciplinary/create-disciplinary `POST` 
+### <a name="profile"></a> Profile
+#### <a name="/profile/register"></a>/profile/register `POST` 
+#### <a name="/profile/terminated-employee"></a>/profile/terminated-employee `POST` 
+#### <a name="/profile/all"></a>/profile/all `GET` 
+### <a name="role"></a> Role
+#### <a name="/role/create-role"></a>/role/create-role `POST` 
+#### <a name="/role/get-roles"></a>/role/get-roles `GET` 
--- swagger-to-md.py
+++ swagger-to-md.py
@@ -10,14 +10,41 @@ def get_endpoint_groups(endpoints: list):
             groups.append(split[1])
     return groups
 
-def get_endpoints_in_group(all_endpoints: list, group):
+def get_endpoint_titles_in_group(all_endpoints: list, group: str):
     endpoints = []
     for endpoint in all_endpoints:
-        split = endpoint.split("/")[2:]
-        relative_path = "/".join(split)
-        if not relative_path in endpoints:
-            endpoints.append(relative_path)
+        split = endpoint.split("/")
+        if split[1].lower() == group.lower():
+            relative_path = "/".join(split[2:])
+            if not relative_path in endpoints:
+                endpoints.append(relative_path)
     return endpoints
+
+def get_all_endpoint_data_in_group(all_endpoints: list, group: str):
+    endpoints_data = []
+    for endpoint, data in all_endpoints.items():
+        split = endpoint.split("/")
+        if split[1].lower() == group.lower():
+            relative_path = "/".join(split[2:])
+            if data not in endpoints_data:
+                endpoints_data.append({"endpoint": endpoint, "data": data})
+    return endpoints_data
+
+def get_detailed_group_docs(group_title: str, all_endpoints: list):
+    lines = []
+    lines.append(f"### <a name=\"{group_title}\"></a> {group_title.title()}")
+    # for each endpoint in group print detailed docs
+    endpoints_data = get_all_endpoint_data_in_group(all_endpoints, group_title)
+
+    for endpoint_data in endpoints_data:
+        title_line = f"#### <a name=\"{endpoint_data["endpoint"]}\"></a>{endpoint_data["endpoint"]}"
+        for method in endpoint_data["data"].keys():
+            title_line += f" `{method.upper()}` "
+        lines.append(title_line)
+        # for each method right doc
+        
+
+    return lines
         
 
 def main():
@@ -32,29 +59,31 @@ def main():
 
     data = {}
     with open(source_json_file) as file:
-        lines = file.read()
-        data = json.loads(lines)
+        data = json.loads(file.read())
+
+    lines = []
+    lines.append(f"# {data["info"]["title"]} API Document - Version: {data["info"]["version"]}")
+    lines.append("###### OpenAPI Documentation Markdown Document")
+    lines.append(f"Open API Version: {data["openapi"]}")
+    lines.append(f"Number of Paths: {len(data["paths"])}")
+    lines.append("## API Endpoints:")
+    # print end points groups, print endpoints nested
+    groups = get_endpoint_groups(data["paths"])
+    for group in groups:
+        lines.append(f"* [{group.title()}](#{group})")
+        endpoints_in_group = get_endpoint_titles_in_group(data["paths"], group)
+        for endpoint in endpoints_in_group:
+            lines.append(f"\t* {endpoint}")
+
+    
+    # print detailed docs for each group of endpoints
+    for group in groups:
+        for line in get_detailed_group_docs(group, data["paths"]):
+            lines.append(line)
 
     with open(output_file_path, "w") as file:
-        file.write(f"# {data["info"]["title"]} API Document - Version: {data["info"]["version"]}\n")
-        file.write("###### OpenAPI Documentation Markdown Document\n")
-        file.write(f"Open API Version: {data["openapi"]}\n")
-        file.write(f"Number of Paths: {len(data["paths"])}\n")
-        file.write(f"## API Endpoints:\n")
-        # print end points groups, print endpoints nested
-        groups = get_endpoint_groups(data["paths"])
-        for group in groups:
-            file.write(f"* [{group.title()}](#{group})\n")
-            endpoints_in_group = get_endpoints_in_group(data["paths"], group)
-            for endpoint in endpoints_in_group:
-                file.write(f"\t* {endpoint}\n")
-
-        
-        # print detailed docs for each group of endpoints
-        for group in groups:
-            file.write(f"## <a name=\"{group}\"></a> {group.title()}\n")
-            file.write(f"Some detailed documentation for this group will go here.\n")
-
+        for line in lines:
+            file.write(f"{line}\n")
         
     
 
